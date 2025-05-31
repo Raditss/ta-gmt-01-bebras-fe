8ac982865cf3e1df767be3942d2a624396792510
HoseaNA <13521057@std.stei.itb.ac.id>
2025-05-31 18:09:17 +0700

    refactor: use temporary change to access font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type React from "react"
 import type { Metadata } from "next/types"
-import { Inter } from "next/font/google"
+/* 
+  TODO: revert back to use next/font/google
+*/
 import "./globals.css"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "CodeLeaf - Coding Challenge Platform",
@@ -17,7 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" 
+        />
+      </head>
       <body data-gptw="">
           {children}
       </body>
